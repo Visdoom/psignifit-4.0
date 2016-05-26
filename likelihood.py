logLikelihood is fixed. Now the priors are added appropriately. The optimization runs through and returns the right values

likelihood.py
@@ -107,6 +107,7 @@ def logLikelihood(data,options, args):
         scale = 1-gamma -lamb
         psi = np.array([sigmoidHandle(x,alpha, beta) for x in data[:,0]])
         psi = gamma + scale*psi
+        psi = np.squeeze(psi)
         n = np.array(data[:,2])
         k = np.array(data[:,1])
         varscale = varscale**2;
@@ -192,20 +193,37 @@ def logLikelihood(data,options, args):
         
         p = np.concatenate((np.tile(pbin, [1,1,1,1,np.sum(vbinom)]),p), axis=4)
         p[np.isnan(p)] = -np.inf
-
+      
     if (options['priors']):
+        
         if isinstance(options['priors'], list):
             if hasattr(options['priors'][0], '__call__'):
-                p += np.log(options['priors'][0](alpha))
+                prior = np.log(options['priors'][0](alpha))
+                if not(oneParameter):
+                    prior = np.tile(prior, (1,) + p.shape[1:])
+                p += prior
             if hasattr(options['priors'][1], '__call__'):
-                p += np.log(options['priors'][1](beta))
+                prior = np.log(options['priors'][1](beta))
+                if not(oneParameter):
+                    prior = np.tile(prior, (p.shape[0],1) +p.shape[2:])
+                p += prior
             if hasattr(options['priors'][2], '__call__'):
-                p += np.log(options['priors'][2](lamb))
+                prior = np.log(options['priors'][2](lamb))
+                if not(oneParameter):
+                    prior = np.tile(prior,p.shape[0:2] + (1,) + p.shape[3:])
+                p += prior
             if hasattr(options['priors'][3], '__call__'):
-                p += np.log(options['priors'][3](gamma))
+                prior = np.log(options['priors'][3](gamma))
+                if not(oneParameter):
+                    prior = np.tile(prior, p.shape[0:3] +(1,p.shape[4]))
+                p += prior
             if hasattr(options['priors'][4], '__call__'):
-                p += np.log(options['priors'][4](varscaleOrig))
-    
+                prior = np.log(options['priors'][4](varscaleOrig))
+                if not(oneParameter):
+                    prior = np.tile(prior, p.shape[0:-1]+(1,))
+                else:
+                    prior = np.squeeze(prior)
+                p += prior
 
     return p  
 

psignifitCore.py
@@ -118,25 +118,27 @@ def psignifitCore(data, options):
             Fit = scipy.optimize.fmin(fun, x0, args = (a,), xtol=0, ftol = 0, maxiter = 100, maxfun=100)
             warnings.warn('changed options for optimization')
         else:            
-            Fit = scipy.optimize.fmin(fun, x0, args = (a,), disp = False)
+            Fit = scipy.optimize.fmin(fun, x0, args = (a,), disp = True)
           
         if options['expType'] == 'YesNo':
             result['Fit'] = deepcopy(Fit)
-        elif options['expType'] == 'nAFC': #TODO is this row or column vectors?
+        elif options['expType'] == 'nAFC': 
             fit = deepcopy(Fit[0:3])
             fit = np.append(fit, np.array([1/options['expN']]))
             fit = np.append(fit, deepcopy(Fit[3]))
-            result['Fit'] = fit.transpose()
+            result['Fit'] = fit
+            
         elif options['expType'] =='equalAsymptote':
             fit = deepcopy(Fit[0:3])
             fit = np.append(fit, Fit[2])
             fit = np.append(fit, Fit[3])
-            result['Fit'] = fit.transpose()
+            result['Fit'] = fit
         else:
             raise ValueError('unknown expType')
     
         par_idx = np.where(np.isnan(options['fixedPars']) == False)
-        result['Fit'][par_idx] = options['fixedPars'][par_idx] #TODO check
+        for idx in par_idx:
+            result['Fit'][idx] = options['fixedPars'][idx]
             
     elif options['estimateType'] == 'mean':
         # get mean estimate

test.py
@@ -68,9 +68,8 @@ options['logspace']    = 0
 temp_data= importer.loadmat('variables.mat', struct_as_record=True,matlab_compatible=True)
 temp_options = importer.loadmat('options.mat', struct_as_record=False, squeeze_me=True)
 
-test_data = np.array([0.001,45,90])
 var = np.array([ 0.00466446,  0.00473373,  0,  0.5 ,0])
 
-- logLikelihood(test_data, options, var)
+- logLikelihood(data, options, var)
 
 